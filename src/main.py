8b447fa19fef4fbac3785a7a22f07c66102a3d5d
Raheel Abdul Rehman <70536721+Raheel31@users.noreply.github.com>
2025-09-04 01:04:53 +0530

    Updates

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import pandas as pd
 from fastapi import FastAPI, Query
 import uvicorn
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from model import recommend_songs as model1 # pylint: disable=import-error
 from logger import get_logger # pylint: disable=import-error
 
